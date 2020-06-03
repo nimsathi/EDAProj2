5206c58d2f38a4c21c56f803b5b0019aa76f4d88
Nim Sathiamoorthy <nim.sathi@gmail.com>
2020-06-03 12:41:51 +0100

    initial commit

---
+++ plot3.R
@@ -0,0 +1,18 @@
+library(dplyr)
+library(ggplot2)
+
+NEI <- readRDS("summarySCC_PM25.rds")
+
+# Baltimore PM2.5 emissions grouped by year and point type
+summaryData <- NEI %>%
+  filter(fips == "24510") %>%
+  group_by(year, type) %>%
+  summarise(totalEmissions = sum(Emissions));
+
+g <- ggplot(data=summaryData, aes(x=year, y=totalEmissions, color=type)) +
+  geom_point() +
+  geom_smooth(method="lm") + 
+  facet_wrap(~ type) +
+  labs(title="Baltimore - total PM2.5 emissions by type", x="Year", y="PM2.5 Emissions (T)")
+
+  
